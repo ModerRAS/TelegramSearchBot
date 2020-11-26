6caf1054b7ebdb95ae1b0af5bde446359272baa9
ModerRAS <adqewsx@gmail.com>
2020-11-26 20:39:42 +0800

    将`重建索引`的实现放入Service中

--- TelegramSearchBot/Controller/RefreshController.cs
+++ TelegramSearchBot/Controller/RefreshController.cs
@@ -15,8 +15,14 @@ using TelegramSearchBot.Model;
 namespace TelegramSearchBot.Controller {
     class RefreshController : IOnMessage {
         private readonly SearchContext context;
+        private readonly IMessageService messageService;
         private readonly IDistributedCache Cache;
-        public RefreshController(ITelegramBotClient botClient, SearchContext context, IDistributedCache Cache) : base(botClient) {
+        public RefreshController(ITelegramBotClient botClient, 
+                                 SearchContext context, 
+                                 IDistributedCache Cache,
+                                 IMessageService messageService
+            ) : base(botClient) {
+            this.messageService = messageService;
             this.context = context;
             this.Cache = Cache;
         }
@@ -35,22 +41,28 @@ namespace TelegramSearchBot.Controller {
                 Command = e.Message.Caption;
             } else return;
             if (Command.Length == 4 && Command.Equals("重建索引")) {
-                using (var sonicIngestConnection = NSonicFactory.Ingest(Env.SonicHostname, Env.SonicPort, Env.SonicSecret)) {
-                    await sonicIngestConnection.ConnectAsync();
+                var messages = from s in context.Messages
+                               select s;
 
-                    var messages = from s in context.Messages
-                                   select s;
+                var users = (from s in context.Users
+                             select s).ToList();
 
-                    var users = (from s in context.Users
-                                 select s).ToList();
+                foreach (var message in messages) {
+                    await messageService.ExecuteAsync(new MessageOption() {
+                        ChatId = message.GroupId,
+                        MessageId = message.MessageId,
+                        UserId = message.GroupId,
+                        Content = message.Content
+                    });
 
-                    foreach (var message in messages) {
-                        await sonicIngestConnection.PushAsync(Env.SonicCollection, message.GroupId.ToString(), $"{message.GroupId}:{message.MessageId}", message.Content);
-
-                        foreach (var user in users) {
-                            if (user.GroupId.Equals(message.GroupId)) {
-                                await sonicIngestConnection.PushAsync(Env.SonicCollection, user.UserId.ToString(), $"{message.GroupId}:{message.MessageId}", message.Content);
-                            }
+                    foreach (var user in users) {
+                        if (user.GroupId.Equals(message.GroupId)) {
+                            await messageService.ExecuteAsync(new MessageOption() { 
+                                ChatId = message.GroupId,
+                                MessageId = message.MessageId,
+                                UserId = user.UserId,
+                                Content = message.Content
+                            });
                         }
                     }
                 }
