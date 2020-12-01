bcf6caa2dcde7b7c54bc6bafec3bbca0829520a0
ModerRAS <adqewsx@gmail.com>
2020-12-01 17:00:06 +0800

    给`MessageService`添加非空检测

--- TelegramSearchBot/Service/MessageService.cs
+++ TelegramSearchBot/Service/MessageService.cs
@@ -58,7 +58,9 @@ namespace TelegramSearchBot.Service {
 
                 foreach (var e in Users) {
                     foreach (var s in SplitWords(messageOption.Content)) {
-                        await sonicIngestConnection.PushAsync(Env.SonicCollection, e.ToString(), $"{messageOption.ChatId}:{messageOption.MessageId}", s);
+                        if (!string.IsNullOrEmpty(s)) {
+                            await sonicIngestConnection.PushAsync(Env.SonicCollection, e.ToString(), $"{messageOption.ChatId}:{messageOption.MessageId}", s);
+                        }
                     }
                     
                 }
