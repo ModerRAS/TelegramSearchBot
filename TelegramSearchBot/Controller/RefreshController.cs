f912749cfd16fea3a15e51df1fa4860ce80f7e63
ModerRAS <adqewsx@gmail.com>
2021-01-06 20:19:32 +0800

    性能优化

--- TelegramSearchBot/Controller/RefreshController.cs
+++ TelegramSearchBot/Controller/RefreshController.cs
@@ -17,85 +17,16 @@ using TelegramSearchBot.Service;
 
 namespace TelegramSearchBot.Controller {
     class RefreshController : IOnMessage {
-        private readonly SearchContext context;
         private readonly RefreshService refreshService;
-        private readonly IDistributedCache Cache;
-        private readonly SendMessage Send;
         public RefreshController(SearchContext context, 
                                  IDistributedCache Cache,
                                  RefreshService refreshService, 
                                  SendMessage Send
             ) {
-            this.Send = Send;
             this.refreshService = refreshService;
-            this.context = context;
-            this.Cache = Cache;
         }
 
-        private async void RebuildIndex() {
-            var messages = from s in context.Messages
-                           select s;
-            long count = messages.LongCount();
-            await Send.Log($"共{count}条消息，现在开始重建索引");
-            long i = 0;
-            foreach (var message in messages) {
-                if (i % 10000 == 0) {
-                    await Send.Log($"已完成{i * 100 / count}%");
-                }
-                await refreshService.ExecuteAsync(new MessageOption() {
-                    ChatId = message.GroupId,
-                    MessageId = message.MessageId,
-                    UserId = message.GroupId,
-                    Content = message.Content
-                });
-                i++;
-            }
-            await Send.Log("重建索引完成");
-        }
-
-        private async void RefreshCache() {
-            var messages = from s in context.Messages
-                           select s;
-            long count = messages.LongCount();
-            await Send.Log($"共{count}条消息，现在开始刷新缓存");
-            long i = 0;
-            foreach (var message in messages) {
-                if (i % 10000 == 0) {
-                    await Send.Log($"已完成{i * 100 / count}%");
-                }
-                await Cache.SetAsync(
-                    $"{message.GroupId}:{message.MessageId}",
-                    Encoding.UTF8.GetBytes(message.Content),
-                    new DistributedCacheEntryOptions { });
-                i++;
-            }
-            await Send.Log("刷新缓存完成");
-        }
-
-        private async void RefreshAll() {
-            var messages = from s in context.Messages
-                           select s;
-            long count = messages.LongCount();
-            await Send.Log($"共{count}条消息，现在开始全部刷新");
-            long i = 0;
-            foreach (var message in messages) {
-                if (i % 10000 == 0) {
-                    await Send.Log($"已完成{i * 100 / count}%");
-                }
-                await refreshService.ExecuteAsync(new MessageOption() {
-                    ChatId = message.GroupId,
-                    MessageId = message.MessageId,
-                    UserId = message.GroupId,
-                    Content = message.Content
-                });
-                await Cache.SetAsync(
-                    $"{message.GroupId}:{message.MessageId}",
-                    Encoding.UTF8.GetBytes(message.Content),
-                    new DistributedCacheEntryOptions { });
-                i++;
-            }
-            await Send.Log("全部刷新完成");
-        }
+        
 
         public async Task ExecuteAsync(object sender, MessageEventArgs e) {
             if (e.Message.Chat.Id < 0) {
@@ -110,15 +41,7 @@ namespace TelegramSearchBot.Controller {
             } else if (!string.IsNullOrEmpty(e.Message.Caption)) {
                 Command = e.Message.Caption;
             } else return;
-            if (Command.Length == 4 && Command.Equals("重建索引")) {
-                RebuildIndex();
-            }
-            if (Command.Length == 4 && Command.Equals("刷新缓存")) {
-                RefreshCache();
-            }
-            if (Command.Length == 4 && Command.Equals("全部刷新")) {
-                RefreshAll();
-            }
+            await refreshService.ExecuteAsync(Command);
         }
 
     }
--- TelegramSearchBot/Service/RefreshService.cs
+++ TelegramSearchBot/Service/RefreshService.cs
@@ -14,38 +14,121 @@ namespace TelegramSearchBot.Service {
         public RefreshService(SearchContext context, IDistributedCache Cache, SendMessage Send) : base(context, Cache, Send) {
         }
 
-        public async Task ExecuteAsync(MessageOption messageOption) {
-
+        private async Task RebuildIndex() {
+            var messages = from s in context.Messages
+                           select s;
+            long count = messages.LongCount();
+            await Send.Log($"共{count}条消息，现在开始重建索引");
+            long i = 0;
             using (var sonicIngestConnection = NSonicFactory.Ingest(Env.SonicHostname, Env.SonicPort, Env.SonicSecret)) {
                 await sonicIngestConnection.ConnectAsync();
-
-                var UsersQuery = from s in context.Users
-                                 where s.GroupId.Equals(messageOption.ChatId)
-                                 select s.UserId;
-
-                var Users = UsersQuery.ToList();
-                Users.Add(messageOption.ChatId);
-
-
-                try {
-                    foreach (var e in Users) {
-                        foreach (var s in SplitWords(messageOption.Content)) {
-                            if (!string.IsNullOrEmpty(s)) {
-                                await sonicIngestConnection.PushAsync(Env.SonicCollection, e.ToString(), $"{messageOption.ChatId}:{messageOption.MessageId}", s);
-                            }
-                        }
+                foreach (var message in messages) {
+                    if (i % 10000 == 0) {
+                        await Send.Log($"已完成{i * 100 / count}%");
                     }
-                } catch (AssertionException exception) {
-                    await Send.Log($"{messageOption.ChatId}:{messageOption.MessageId}\n{messageOption.Content}");
-                    await Send.Log(exception.ToString());
-                    Console.Error.WriteLine(exception);
+                    await ExecuteAsync(new MessageOption() {
+                        ChatId = message.GroupId,
+                        MessageId = message.MessageId,
+                        UserId = message.GroupId,
+                        Content = message.Content
+                    }, sonicIngestConnection);
+                    i++;
                 }
 
-                await Cache.SetAsync(
-                    $"{messageOption.ChatId}:{messageOption.MessageId}", 
-                    Encoding.UTF8.GetBytes(messageOption.Content.Replace("\n", " ")), 
-                    new DistributedCacheEntryOptions { });
             }
+            
+            await Send.Log("重建索引完成");
+        }
+
+        private async Task RefreshCache() {
+            var messages = from s in context.Messages
+                           select s;
+            long count = messages.LongCount();
+            await Send.Log($"共{count}条消息，现在开始刷新缓存");
+            long i = 0;
+            foreach (var message in messages) {
+                if (i % 10000 == 0) {
+                    await Send.Log($"已完成{i * 100 / count}%");
+                }
+                await Cache.SetAsync(
+                    $"{message.GroupId}:{message.MessageId}",
+                    Encoding.UTF8.GetBytes(message.Content),
+                    new DistributedCacheEntryOptions { });
+                i++;
+            }
+            await Send.Log("刷新缓存完成");
+        }
+
+        private async Task RefreshAll() {
+            var messages = from s in context.Messages
+                           select s;
+            long count = messages.LongCount();
+            await Send.Log($"共{count}条消息，现在开始全部刷新");
+            long i = 0;
+            using (var sonicIngestConnection = NSonicFactory.Ingest(Env.SonicHostname, Env.SonicPort, Env.SonicSecret)) {
+                await sonicIngestConnection.ConnectAsync();
+                foreach (var message in messages) {
+                    if (i % 10000 == 0) {
+                        await Send.Log($"已完成{i * 100 / count}%");
+                    }
+                    await ExecuteAsync(new MessageOption() {
+                        ChatId = message.GroupId,
+                        MessageId = message.MessageId,
+                        UserId = message.GroupId,
+                        Content = message.Content
+                    }, sonicIngestConnection);
+                    await Cache.SetAsync(
+                        $"{message.GroupId}:{message.MessageId}",
+                        Encoding.UTF8.GetBytes(message.Content),
+                        new DistributedCacheEntryOptions { });
+                    i++;
+                }
+
+            }
+            
+            await Send.Log("全部刷新完成");
+        }
+
+        public async Task ExecuteAsync(string Command) {
+            if (Command.Length == 4 && Command.Equals("重建索引")) {
+                await RebuildIndex();
+            }
+            if (Command.Length == 4 && Command.Equals("刷新缓存")) {
+                await RefreshCache();
+            }
+            if (Command.Length == 4 && Command.Equals("全部刷新")) {
+                await RefreshAll();
+            }
+        }
+
+        public async Task ExecuteAsync(MessageOption messageOption, ISonicIngestConnection sonicIngestConnection) {
+            var UsersQuery = from s in context.Users
+                             where s.GroupId.Equals(messageOption.ChatId)
+                             select s.UserId;
+
+            var Users = UsersQuery.ToList();
+            Users.Add(messageOption.ChatId);
+
+
+            try {
+                foreach (var e in Users) {
+                    foreach (var s in SplitWords(messageOption.Content)) {
+                        if (!string.IsNullOrEmpty(s)) {
+                            await sonicIngestConnection.PushAsync(Env.SonicCollection, e.ToString(), $"{messageOption.ChatId}:{messageOption.MessageId}", s);
+                        }
+                    }
+                }
+            } catch (AssertionException exception) {
+                await Send.Log($"{messageOption.ChatId}:{messageOption.MessageId}\n{messageOption.Content}");
+                await Send.Log(exception.ToString());
+                Console.Error.WriteLine(exception);
+            }
+
+            await Cache.SetAsync(
+                $"{messageOption.ChatId}:{messageOption.MessageId}",
+                Encoding.UTF8.GetBytes(messageOption.Content.Replace("\n", " ")),
+                new DistributedCacheEntryOptions { });
+            
             
 
 
