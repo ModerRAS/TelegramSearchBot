360aeed562fca325473a62b9e5974477205e6b27
ModerRAS <adqewsx@gmail.com>
2021-02-10 10:17:21 +0800

    加一个图像灰度化处理

--- TelegramSearchBot/Service/AutoOCRService.cs
+++ TelegramSearchBot/Service/AutoOCRService.cs
@@ -6,9 +6,41 @@ using System.Text;
 using System.Drawing;
 using System.Threading.Tasks;
 using Tesseract;
+using System.Drawing.Imaging;
 
 namespace TelegramSearchBot.Service {
     class AutoOCRService {
+        private async Task<Bitmap> ConvertToGray(Bitmap rgb_img) {
+            Bitmap newBitmap = new Bitmap(rgb_img.Width, rgb_img.Height);
+
+            //get a graphics object from the new image
+            using (Graphics g = Graphics.FromImage(newBitmap)) {
+
+                //create the grayscale ColorMatrix
+                ColorMatrix colorMatrix = new ColorMatrix(
+                   new float[][]
+                   {
+             new float[] {.3f, .3f, .3f, 0, 0},
+             new float[] {.59f, .59f, .59f, 0, 0},
+             new float[] {.11f, .11f, .11f, 0, 0},
+             new float[] {0, 0, 0, 1, 0},
+             new float[] {0, 0, 0, 0, 1}
+                   });
+
+                //create some image attributes
+                using (ImageAttributes attributes = new ImageAttributes()) {
+
+                    //set the color matrix attribute
+                    attributes.SetColorMatrix(colorMatrix);
+
+                    //draw the original image on the new image
+                    //using the grayscale color matrix
+                    g.DrawImage(rgb_img, new Rectangle(0, 0, rgb_img.Width, rgb_img.Height),
+                                0, 0, rgb_img.Width, rgb_img.Height, GraphicsUnit.Pixel, attributes);
+                }
+            }
+            return newBitmap;
+        }
         /// <summary>
         /// 按理说是进来文件出去字符的
         /// </summary>
@@ -17,7 +49,8 @@ namespace TelegramSearchBot.Service {
         public async Task<string> ExecuteAsync(MemoryStream file) {
             var stream = new MemoryStream();
             var tg_img = Image.FromStream(file);
-            tg_img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            var bitmap = await ConvertToGray(new Bitmap(tg_img));
+            bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
             stream.Position = 0;
             var texts = new List<string>();
             using (var engine = new TesseractEngine(@"/app/tessdata", "chi_sim", EngineMode.Default)) {
