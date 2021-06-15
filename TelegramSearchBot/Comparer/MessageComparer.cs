fa40785138b17cb719f609c971f2b574c13c3f14
ModerRAS <adqewsx@gmail.com>
2021-06-15 16:36:47 +0800

    添加hashset比较方法

--- TelegramSearchBot/Comparer/MessageComparer.cs
+++ TelegramSearchBot/Comparer/MessageComparer.cs
@@ -9,7 +9,7 @@ using TelegramSearchBot.Model;
 namespace TelegramSearchBot.Comparer {
     class MessageComparer : IEqualityComparer<Message> {
         public bool Equals(Message x, Message y) {
-            return x.Id == y.Id && x.GroupId == y.GroupId && x.MessageId == y.MessageId;
+            return x.GroupId == y.GroupId && x.MessageId == y.MessageId;
         }
 
         public int GetHashCode([DisallowNull] Message obj) {
