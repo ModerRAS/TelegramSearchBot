2a5696a5f7cc640e0773b6bae629acb32259ec82
ModerRAS <adqewsx@gmail.com>
2021-10-04 22:01:18 +0800

    清除开头和结尾的空格

--- TelegramSearchBot/Controller/AutoOCRController.cs
+++ TelegramSearchBot/Controller/AutoOCRController.cs
@@ -13,13 +13,11 @@ using TelegramSearchBot.Service;
 namespace TelegramSearchBot.Controller {
     class AutoOCRController : IOnMessage {
         private readonly PaddleOCRService paddleOCRService;
-        private readonly SendMessage Send;
         private readonly MessageService messageService;
         private readonly ITelegramBotClient botClient;
         public AutoOCRController(ITelegramBotClient botClient, PaddleOCRService paddleOCRService, SendMessage Send, MessageService messageService) {
             this.paddleOCRService = paddleOCRService;
             this.messageService = messageService;
-            this.Send = Send;
             this.botClient = botClient;
         }
         public async Task ExecuteAsync(object sender, MessageEventArgs e) {
@@ -51,7 +49,7 @@ namespace TelegramSearchBot.Controller {
                     ChatId = e.Message.Chat.Id,
                     MessageId = e.Message.MessageId,
                     UserId = e.Message.From.Id,
-                    Content = string.Join(" ", links)
+                    Content = string.Join(" ", links).Trim()
                 });
             }
         }
