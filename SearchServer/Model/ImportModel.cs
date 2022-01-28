c0b6b3454063729aad6f96391a7fa26b3a89a499
ModerRAS <adqewsx@gmail.com>
2022-01-28 16:14:31 +0800

    将Model挪到公共区域

--- SearchServer/Model/ImportModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace TelegramSearchBot.Model {
-    public class ImportModel {
-        public long GroupId { get; set; }
-        public Dictionary<long, string> Messages { get; set; }
-    }
-}
