b06a9fdff5b37cacb64998988ac131129cbeb447
ModerRAS <adqewsx@gmail.com>
2023-08-27 17:34:29 +0800

    JobManager暂时放弃持久化

--- TelegramSearchBot/Manager/JobManager.cs
+++ TelegramSearchBot/Manager/JobManager.cs
@@ -1,5 +1,6 @@
 ﻿using LiteDB;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TelegramSearchBot.Common.Interface;
@@ -8,32 +9,11 @@ namespace TelegramSearchBot.Manager {
     public class JobManager<S, R> where S:ICompareRPC where R:ICompareRPC {
         private ConcurrentQueue<S> SQueue { get; init; }
         private ConcurrentQueue<R> RQueue { get; init; }
-        private LiteDatabase Cache { get; init; } = Env.Cache;
-        private ILiteCollection<ConcurrentQueue<S>> SQ { get; set; }
-        private ILiteCollection<ConcurrentQueue<R>> RQ { get; set; }
         public JobManager() {
-            SQ = Cache.GetCollection<ConcurrentQueue<S>>("SQueue");
-            RQ = Cache.GetCollection<ConcurrentQueue<R>>("RQueue");
-            var SQList = SQ.FindAll().ToList();
-            if (SQList.Count > 0) {
-                SQueue = SQList[0];
-            } else {
-                SQueue = new ConcurrentQueue<S>();
-            }
-            var RQList = RQ.FindAll().ToList();
-            if (RQList.Count > 0) {
-                RQueue = RQList[0];
-            } else {
-                RQueue = new ConcurrentQueue<R>();
-            }
+            SQueue = new ConcurrentQueue<S>();
+            RQueue = new ConcurrentQueue<R>();
         }
         public async Task WaitAndSave() {
-            var SQ = Cache.GetCollection<ConcurrentQueue<S>>("SQueue");
-            SQ.DeleteAll();
-            SQ.Insert(SQueue);
-            var RQ = Cache.GetCollection<ConcurrentQueue<R>>("RQueue");
-            RQ.DeleteAll();
-            RQ.Insert(RQueue);
             await Task.Delay(Env.TaskDelayTimeout);
         }
         public async Task<R> Execute(S item) {
