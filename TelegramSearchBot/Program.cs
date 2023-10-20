7ffe06ea40fde8fd041d1c3065dd2f1453f68117
ModerRAS <adqewsx@gmail.com>
2023-10-20 17:01:18 +0800

    Revert "增加OCRHub类与Agent通讯，修改项目为Asp.Net Core项目"
    This reverts commit d02d5f6cd99ca2c14dee01eb3215b38668f62f0c.
    
    # Conflicts:
    #	TelegramSearchBot/Hubs/OCRHub.cs
    #	TelegramSearchBot/Program.cs
    #	TelegramSearchBot/TelegramSearchBot.csproj

--- TelegramSearchBot/Program.cs
+++ TelegramSearchBot/Program.cs
@@ -1,13 +1,20 @@
 ﻿using LiteDB;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging.Debug;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Args;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -15,76 +22,51 @@ using TelegramSearchBot.Controller;
 using TelegramSearchBot.Intrerface;
 using TelegramSearchBot.Manager;
 using TelegramSearchBot.Service;
-using MessagePack;
-using Microsoft.AspNetCore.Builder;
-using TelegramSearchBot.Hubs;
-using TelegramSearchBot.Common.Model.DTO;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace TelegramSearchBot {
     class Program {
         private static IServiceProvider service;
-        public static WebApplicationBuilder CreateHostBuilder(string[] args) {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddSingleton<ITelegramBotClient>(sp => new TelegramBotClient(new TelegramBotClientOptions(Env.BotToken, Env.BaseUrl)));
-            builder.Services.AddSingleton<LuceneManager>();
-            builder.Services.AddSingleton<JobManager<OCRTaskPost, OCRTaskResult>>();
-            builder.Services.AddSingleton<ITokenManager, TokenManager>();
-            builder.Services.AddSingleton<SendMessage>();
-            builder.Services.AddSingleton<LiteDbManager>();
-            builder.Services.AddSingleton<OCRHub>();
-            builder.Services.AddSignalR().AddMessagePackProtocol(options => {
-                options.SerializerOptions = MessagePackSerializerOptions.Standard
-                .WithSecurity(MessagePackSecurity.UntrustedData);
-            });
-            //builder.Services.AddRazorPages();
-            AddController(builder.Services);
-            AddService(builder.Services);
-            builder.Logging.ClearProviders();
-            builder.Logging.AddSimpleConsole(options =>
-            {
-                options.IncludeScopes = true;
-                options.SingleLine = true;
-                options.TimestampFormat = "[yyyy/MM/dd HH:mm:ss] ";
-            });
-            return builder;
-        }
-            
-        static async Task Main(string[] args) {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices(service => {
+                    service.AddSingleton<ITelegramBotClient>(sp => new TelegramBotClient(new TelegramBotClientOptions(Env.BotToken, Env.BaseUrl)));
+                    service.AddTransient<SendService>();
+                    service.AddSingleton<SendMessage>();
+                    service.AddSingleton<LuceneManager>();
+                    service.AddTransient<SearchService>();
+                    service.AddTransient<MessageService>();
+                    service.AddTransient<AutoQRService>();
+                    service.AddTransient<RefreshService>();
+                    service.AddTransient<PaddleOCRService>();
+                    AddController(service);
+                });
+        static void Main(string[] args) {
             if (!Directory.Exists(Env.WorkDir)) {
                 Utils.CreateDirectorys(Env.WorkDir);
             }
             Env.Database = new LiteDatabase($"{Env.WorkDir}/Data.db");
             Env.Cache = new LiteDatabase($"{Env.WorkDir}/Cache.db");
             Directory.SetCurrentDirectory(Env.WorkDir);
-            var app = CreateHostBuilder(args).Build();
-            //app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            //app.MapRazorPages();
-            app.UseEndpoints(endpoints => {
-                endpoints.MapHub<OCRHub>("/OCRHub");
-            });
-            service = app.Services;
-            var bot = service.GetRequiredService<ITelegramBotClient>();
+            IHost host = CreateHostBuilder(args)
+                .ConfigureLogging(logging => {
+                    logging.ClearProviders();
+                    logging.AddSimpleConsole(options =>
+                    {
+                        options.IncludeScopes = true;
+                        options.SingleLine = true;
+                        options.TimestampFormat = "[yyyy/MM/dd HH:mm:ss] ";
+                    });
+                }).Build();
+            var bot = host.Services.GetRequiredService<ITelegramBotClient>();
             using CancellationTokenSource cts = new();
             bot.StartReceiving(
                 HandleUpdateAsync(service), 
                 HandleErrorAsync(service), new() {
                     AllowedUpdates = Array.Empty<UpdateType>() // receive all update types
             }, cts.Token);
-            var wait = app.RunAsync();
-            
-            var tasks = InitController(service);
-            await wait;
-            foreach (var task in tasks) {
-                await task;
-            }
+            service = host.Services;
+            InitController(host.Services);
+            host.Run();
         }
         public static void AddController(IServiceCollection service) {
             service.Scan(scan => scan
@@ -109,8 +91,8 @@ namespace TelegramSearchBot {
             );
 
         }
-        public static List<Task> InitController(IServiceProvider service) {
-            return new List<Task>() { service.GetRequiredService<SendMessage>().Run() };
+        public static void InitController(IServiceProvider service) {
+            _ = service.GetRequiredService<SendMessage>().Run();
         }
 
         public static Func<ITelegramBotClient, Update, CancellationToken, Task> HandleUpdateAsync(IServiceProvider service) {
