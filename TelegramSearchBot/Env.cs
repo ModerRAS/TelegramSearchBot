f773ad43887b79ac3c8d9478d0dbbdb8fcaed507
ModerRAS <adqewsx@gmail.com>
2023-11-08 20:55:55 +0800

    将环境变量修改为配置文件

--- TelegramSearchBot/Env.cs
+++ TelegramSearchBot/Env.cs
@@ -1,20 +1,45 @@
 ﻿using LiteDB;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace TelegramSearchBot {
     public static class Env {
-        public static readonly string BaseUrl = Environment.GetEnvironmentVariable("BaseUrl") ?? "https://api.telegram.org";
+        static Env() {
+            WorkDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TelegramSearchBot");
+            if (!Directory.Exists(WorkDir)) {
+                Directory.CreateDirectory(WorkDir);
+            }
+            var config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(Path.Combine(WorkDir, "Config.json")));
+            BaseUrl = config.BaseUrl;
+            IsLocalAPI = config.IsLocalAPI;
+            BotToken = config.BotToken;
+            AdminId = config.AdminId;
+            EnableAutoOCR = config.EnableAutoOCR;
+            //WorkDir = config.WorkDir;
+            TaskDelayTimeout = config.TaskDelayTimeout;
+        }
+        public static readonly string BaseUrl;
 #pragma warning disable CS8604 // 引用类型参数可能为 null。
-        public static readonly bool IsLocalAPI = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("IsLocalAPI")) && bool.Parse(Environment.GetEnvironmentVariable("IsLocalAPI"));
+        public static readonly bool IsLocalAPI;
 #pragma warning restore CS8604 // 引用类型参数可能为 null。
-        public static readonly string BotToken = Environment.GetEnvironmentVariable("BotToken") ?? string.Empty;
-        public static readonly long AdminId = long.Parse(Environment.GetEnvironmentVariable("AdminId") ?? string.Empty);
-        public static readonly bool EnableAutoOCR = (Environment.GetEnvironmentVariable("EnableAutoOCR") ?? string.Empty).Equals("true");
-        public static readonly string WorkDir = Environment.GetEnvironmentVariable("WorkDir") ?? "/data/TelegramSearchBot";
-        public static readonly int TaskDelayTimeout = int.Parse(Environment.GetEnvironmentVariable("TaskDelayTimeout") ?? "1000");
+        public static readonly string BotToken;
+        public static readonly long AdminId;
+        public static readonly bool EnableAutoOCR;
+        public static readonly string WorkDir;
+        public static readonly int TaskDelayTimeout;
         public static LiteDatabase Database { get; set; }
         public static LiteDatabase Cache { get; set; }
+    }
+    public class Config {
+        public string BaseUrl { get; set; } = "https://api.telegram.org";
+        public string BotToken { get; set; }
+        public long AdminId { get; set; }
+        public bool EnableAutoOCR { get; set; } = false;
+        //public string WorkDir { get; set; } = "/data/TelegramSearchBot";
+        public bool IsLocalAPI { get; set; } = false;
+        public int TaskDelayTimeout { get; set; } = 1000;
     }
 }
