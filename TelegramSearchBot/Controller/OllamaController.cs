90755fe47da91a5061b22d1706d1b33af5a03540
ModerRAS <adqewsx@gmail.com>
2024-12-26 16:34:04 +0800

    添加Ollama相关依赖，Ollama相关实现未完成

---
+++ TelegramSearchBot/Controller/OllamaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using TelegramSearchBot.Intrerface;
+
+namespace TelegramSearchBot.Controller {
+    public class OllamaController : IOnUpdate {
+        private readonly ILogger logger;
+        public ITelegramBotClient botClient { get; set; }
+        public OllamaController(ITelegramBotClient botClient, ILogger<OllamaController> logger) {
+            this.logger = logger;
+            this.botClient = botClient;
+
+        }
+        public async Task ExecuteAsync(Update e) {
+            var BotName = (await botClient.GetMeAsync()).Username;
+            var Message = string.IsNullOrEmpty(e.Message.Text) ? e.Message.Caption : e.Message.Text;
+            if (Message.Contains(BotName)) {
+
+            }
+        }
+    }
+}
---
+++ TelegramSearchBot/Service/OllamaService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using OllamaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramSearchBot.Intrerface;
+
+namespace TelegramSearchBot.Service {
+    public class OllamaService : IService {
+        public string ServiceName => "OllamaService";
+
+        private readonly string ModelName = "qwen2.5:72b-instruct-q2_K";
+        private readonly string Host = "http://localhost:11434";
+        private readonly ILogger _logger;
+        public OllamaApiClient ollama { get; set; }
+
+        public OllamaService(ILogger<OllamaService> logger) {
+            _logger = logger;
+            // set up the client
+            var uri = new Uri(Host);
+            ollama = new OllamaApiClient(uri);
+
+            // select a model which should be used for further operations
+            ollama.SelectedModel = ModelName;
+
+        }
+
+        public bool CheckIfExists(IEnumerable<OllamaSharp.Models.Model> models) {
+            foreach (var model in models) {
+                if (model.Name.Equals(ModelName)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public async Task ExecAsync(string InputToken) {
+            var models = await ollama.ListLocalModelsAsync();
+            if (!CheckIfExists(models)) {
+                await foreach (var status in ollama.PullModelAsync(ModelName))
+                    _logger.LogInformation($"{status.Percent}% {status.Status}");
+            }
+            
+        }
+    }
+}
