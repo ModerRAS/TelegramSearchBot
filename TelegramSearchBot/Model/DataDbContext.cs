9451ec61d92a5bc6feb29d7f9ef6debf8ff70dec
ModerRAS <adqewsx@gmail.com>
2024-12-31 11:14:16 +0800

    将数据库日志输出到Serilog中

--- TelegramSearchBot/Model/DataDbContext.cs
+++ TelegramSearchBot/Model/DataDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,7 @@ namespace TelegramSearchBot.Model {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             // 配置数据库连接字符串
             optionsBuilder.UseSqlite($"Data Source={Env.WorkDir}/Data.sqlite");
+            optionsBuilder.LogTo(Log.Logger.Information, LogLevel.Information);
         }
         public DbSet<Message> Messages { get; set; }
         public DbSet<UserWithGroup> UsersWithGroup { get; set; }
