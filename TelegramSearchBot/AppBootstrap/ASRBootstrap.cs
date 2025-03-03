3b4c119ad13d94f5510cbee3d7ebba38b3b4511a
ModerRAS <adqewsx@gmail.com>
2025-03-03 10:55:51 +0800

    语音识别多进程实现

--- TelegramSearchBot/AppBootstrap/ASRBootstrap.cs
+++ TelegramSearchBot/AppBootstrap/ASRBootstrap.cs
@@ -10,111 +10,41 @@ using System.Threading.Tasks;
 using TelegramSearchBot.Manager;
 using Whisper.net.Ggml;
 using Whisper.net;
+using StackExchange.Redis;
+using Serilog;
+using TelegramSearchBot.Intrerface;
 
 namespace TelegramSearchBot.AppBootstrap {
     public class ASRBootstrap : AppBootstrap {
-        const string modelName = "ggml-medium.bin";
-        public string modelsDir { get; set; }
-        public string modelPath { get; set; }
-
-        const GgmlType ggmlType = GgmlType.Medium;
-        private ILogger<WhisperManager> logger { get; set; }
-
-        private static SemaphoreSlim semaphore = new SemaphoreSlim(1);
-        public ASRBootstrap() {
-            modelsDir = Path.Combine(Env.WorkDir, "Models");
-            modelPath = Path.Combine(modelsDir, modelName);
-        }
-
-        async Task DownloadModelAsync(GgmlType modelType, string modelFileName, string targetModelsDir) {
-            if (!Directory.Exists(targetModelsDir)) {
-                Directory.CreateDirectory(targetModelsDir);
-            }
-            Console.WriteLine($"Model {modelName} not found. Downloading...");
-            await using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(modelType);
-            await using var fileWriter = File.OpenWrite(Path.Combine(targetModelsDir, modelName));
-            await modelStream.CopyToAsync(fileWriter);
-            Console.WriteLine($"Model {modelName} downloaded to {targetModelsDir}");
-        }
-
-        public async Task<string> DetectAsync(Stream wavStream) {
-
-            TimeSpan timeTaken;
-            var startTime = DateTime.UtcNow;
-            if (!File.Exists(modelPath)) {
-                await DownloadModelAsync(ggmlType, modelName, modelsDir);
-                timeTaken = DateTime.UtcNow - startTime;
-                logger.LogInformation($"Time Taken to Download: {timeTaken.TotalSeconds} Seconds");
-            }
-            using var whisperFactory = WhisperFactory.FromPath(modelPath);
-
-            // This section creates the processor object which is used to process the audio file, it uses language `auto` to detect the language of the audio file.
-            await using var processor = whisperFactory.CreateBuilder()
-                                                      .WithThreads(16)
-                                                      //.WithLanguage("zh")
-                                                      .WithLanguageDetection()
-                                                      //.WithPrompt(prompt)
-                                                      .Build();
-
-            timeTaken = DateTime.UtcNow - startTime;
-            logger.LogInformation("Time Taken to init Whisper: {0}", timeTaken.ToLongString());
-
-
-            // This section sets the wavStream to the beginning of the stream. (This is required because the wavStream was written to in the previous section)
-            wavStream.Seek(0, SeekOrigin.Begin);
-
-            logger.LogInformation("⟫ Starting Whisper processing...");
-
-            startTime = DateTime.UtcNow;
-
-            var ToReturn = new List<string>();
-
-            // This section processes the audio file and prints the results (start time, end time and text) to the console.
-            var startId = 1;
-            string lastText = string.Empty;
-            var repeatCount = 0;
-            var cts = new CancellationTokenSource();
-            var token = cts.Token;
-            try {
-                await foreach (var result in processor.ProcessAsync(wavStream, token)) {
-                    timeTaken = DateTime.UtcNow - startTime;
-                    logger.LogInformation($"{result.Start.ToLongString()}-->{result.End.ToLongString()}: {result.Text,-150} [{timeTaken.ToLongString()}]");
-                    ToReturn.Add($"{startId}");
-                    ToReturn.Add($"{result.Start.ToString(@"hh\:mm\:ss\,fff")} --> {result.End.ToString(@"hh\:mm\:ss\,fff")}");
-                    ToReturn.Add($"{result.Text}\n");
-                    startId++;
-                    startTime = DateTime.UtcNow;
-                    if (lastText.Equals(result.Text.Trim())) {
-                        repeatCount++;
-                    } else {
-                        repeatCount = 0;
-                        lastText = result.Text.Trim();
-                    }
-                    if (repeatCount > 100) {
-                        cts.Cancel();
-                    }
+        public static async Task Process(string[] args) {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect($"localhost:{args[1]}");
+            IDatabase db = redis.GetDatabase();
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information() // 设置最低日志级别
+            .WriteTo.Console(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+            .WriteTo.File($"{Env.WorkDir}/logs/log-.txt",
+              rollingInterval: RollingInterval.Day,
+              outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+            .CreateLogger();
+            var asr = new WhisperManager(new LoggerFactory().AddSerilog().CreateLogger<WhisperManager>());
+            var before = DateTime.UtcNow;
+            while (DateTime.UtcNow - before < TimeSpan.FromMinutes(10) ||
+                   db.ListLength("ASRTasks") > 0) {
+                if (db.ListLength("ASRTasks") == 0) {
+                    Task.Delay(1000).Wait();
+                    continue;
                 }
-            } catch (TaskCanceledException ex) {
-                logger.LogError(ex.ToString());
+
+                var task = db.ListLeftPop("ASRTasks").ToString();
+                var audioPath = db.StringGetDelete($"ASRAudioStream-{task}").ToString();
+                var wave = await IProcessAudio.ConvertToWav(await File.ReadAllBytesAsync(audioPath));
+                using MemoryStream stream = new MemoryStream(wave);
+                var response = await asr.DetectAsync(stream);
+                db.StringSet($"ASRAudioText-{task}", response);
             }
-
-
-            logger.LogInformation("⟫ Completed Whisper processing...");
-
-            return string.Join("\n", ToReturn);
         }
-        public async Task<string> ExecuteAsync(Stream wavStream) {
-            string ret = string.Empty;
-            await semaphore.WaitAsync().ConfigureAwait(false);
-            try {
-                ret = await DetectAsync(wavStream);
-            } catch (Exception ex) {
-                logger.LogError(ex.ToString());
-            }
-            semaphore.Release();
-            return ret;
-        }
-        public static void Startup(string[] args) { 
+        public static void Startup(string[] args) {
+            Process(args).Wait();
         }
     }
 }
