bc2f5c5a2cf474f61ac0e1f71a4cb3c36f417226
ModerRAS <adqewsx@gmail.com>
2025-04-27 11:28:59 +0800

    单元测试找不到类修复

--- TelegramSearchBot.Test/Admin/AdminServiceTests.cs
+++ TelegramSearchBot.Test/Admin/AdminServiceTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TelegramSearchBot.Model;
+using TelegramSearchBot.Model.Data;
 using TelegramSearchBot.Service.Manage;
 
 namespace TelegramSearchBot.Test.Admin
