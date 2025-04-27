760fd0ac598e244331cbdda6795a7a2c1ab56219
ModerRAS <adqewsx@gmail.com>
2025-04-27 10:46:31 +0800

    无用数据标记过时

--- TelegramSearchBot/Model/ExportModel.cs
+++ TelegramSearchBot/Model/ExportModel.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TelegramSearchBot.Model.Data;
 
-namespace TelegramSearchBot.Model {
+namespace TelegramSearchBot.Model
+{
+    [Obsolete]
     public class ExportModel {
         public List<Message> Messages { get; set; }
         public List<UserWithGroup> Users { get; set; }
--- TelegramSearchBot/Model/ImportModel.cs
+++ TelegramSearchBot/Model/ImportModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 namespace TelegramSearchBot.Model {
+    [Obsolete]
     public class ImportModel {
         public long GroupId { get; set; }
         public Dictionary<long, string> Messages { get; set; }
