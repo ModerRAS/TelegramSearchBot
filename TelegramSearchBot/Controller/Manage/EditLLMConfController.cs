ccabe5e9c75e37d1e1e4e041ecd8b3465c44d353
ModerRAS <adqewsx@gmail.com>
2025-05-07 10:29:38 +0800

    添加LLM配置编辑相关的代码

---
+++ TelegramSearchBot/Controller/Manage/EditLLMConfController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using TelegramSearchBot.Intrerface;
+
+namespace TelegramSearchBot.Controller.Manage {
+    public class EditLLMConfController : IOnUpdate {
+        public List<Type> Dependencies => new List<Type>();
+
+        public async Task ExecuteAsync(Update e) {
+            
+        }
+    }
+}
---
+++ TelegramSearchBot/Service/Manage/EditLLMConfService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramSearchBot.Intrerface;
+using TelegramSearchBot.Model;
+using TelegramSearchBot.Model.AI;
+
+namespace TelegramSearchBot.Service.Manage {
+    public class EditLLMConfService : IService {
+        public string ServiceName => "EditLLMConfService";
+        protected readonly DataDbContext DataContext;
+        public EditLLMConfService(DataDbContext context) {
+            DataContext = context;
+        }
+
+        /// <summary>
+        /// 添加一个新的LLM通道到数据库
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="Gateway"></param>
+        /// <param name="ApiKey"></param>
+        /// <param name="Provider"></param>
+        /// <returns></returns>
+        public async Task<bool> AddChannel(string Name, string Gateway, string ApiKey, LLMProvider Provider) {
+            
+
+        }
+
+        public async Task AddModelWithChannel() {
+
+        }
+    }
+}
