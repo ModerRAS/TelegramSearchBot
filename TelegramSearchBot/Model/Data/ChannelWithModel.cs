e64c6b0b6ce9bcc773e20c608ca13c9f94bc7add
ModerRAS <adqewsx@gmail.com>
2025-05-07 08:58:26 +0800

    在ChannelWithModel里添加主键

--- TelegramSearchBot/Model/Data/ChannelWithModel.cs
+++ TelegramSearchBot/Model/Data/ChannelWithModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TelegramSearchBot.Model.Data {
     public class ChannelWithModel {
+        [Key]
         public int Id { get; set; }
         public string ModelName { get; set; }
         public int LLMChannelId { get; set; }
