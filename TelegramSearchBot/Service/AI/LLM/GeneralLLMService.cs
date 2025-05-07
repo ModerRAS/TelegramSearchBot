6e87a83f28ad3df728ba7de85efacf6876e43811
ModerRAS <adqewsx@gmail.com>
2025-05-07 21:40:53 +0800

    将OpenAIController修改成通用的LLMController

---
+++ TelegramSearchBot/Service/AI/LLM/GeneralLLMService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramSearchBot.Intrerface;
+
+namespace TelegramSearchBot.Service.AI.LLM {
+    internal class GeneralLLMService : IService {
+        public string ServiceName => "GeneralLLMService";
+    }
+}
