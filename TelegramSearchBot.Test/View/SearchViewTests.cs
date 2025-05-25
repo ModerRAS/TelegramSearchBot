b45801c914c6f07920c66d51cd17ec768bf38cbc
ModerRAS <adqewsx@gmail.com>
2025-05-25 22:42:04 +0800

    将SearchViewTests放在了正确的位置

--- TelegramSearchBot.Test/View/SearchViewTests.cs
+++ TelegramSearchBot.Test/View/SearchViewTests.cs
@@ -1,33 +1,55 @@
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Telegram.Bot;
 using TelegramSearchBot.Model;
+using TelegramSearchBot.Service.BotAPI;
 using TelegramSearchBot.Model.Data;
+using TelegramSearchBot.Manager;
 using TelegramSearchBot.View;
 
-namespace TelegramSearchBot.Tests.View
+namespace TelegramSearchBot.Test.View
 {
     [TestClass]
     public class SearchViewTests
     {
+        private Mock<SendMessage> _sendMessageMock;
+        private Mock<ITelegramBotClient> _botClientMock;
+        private Mock<ILogger<SendMessage>> _loggerMock;
+        private SearchView _searchView;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _botClientMock = new Mock<ITelegramBotClient>();
+            _loggerMock = new Mock<ILogger<SendMessage>>();
+            _sendMessageMock = new Mock<SendMessage>(
+                MockBehavior.Strict,
+                _botClientMock.Object,
+                _loggerMock.Object);
+            _botClientMock = new Mock<ITelegramBotClient>();
+            _searchView = new SearchView(_sendMessageMock.Object, _botClientMock.Object);
+        }
         [TestMethod]
         public void RenderSearchResults_WithResults_ReturnsFormattedString()
         {
             // Arrange
             var messages = new List<Message>
             {
-                new Message { 
+                new Message {
                     Content = "Test message 1",
-                    GroupId = "-100123456789",
+                    GroupId = -100123456789,
                     MessageId = 123
                 },
-                new Message { 
+                new Message {
                     Content = "Test message 2",
-                    GroupId = "-100987654321", 
+                    GroupId = -100987654321,
                     MessageId = 456
                 }
             };
 
-            var searchOption = new SearchOption
+            var searchOption = new TelegramSearchBot.Model.SearchOption
             {
                 Count = 2,
                 Skip = 0,
@@ -35,7 +57,8 @@ namespace TelegramSearchBot.Tests.View
             };
 
             // Act
-            var result = SearchView.RenderSearchResults(messages, searchOption);
+            searchOption.Messages = messages;
+            var result = _searchView.RenderSearchResults(searchOption);
 
             // Assert
             Assert.IsTrue(result.Contains("共找到 2 项结果"));
@@ -50,7 +73,7 @@ namespace TelegramSearchBot.Tests.View
         {
             // Arrange
             var messages = new List<Message>();
-            var searchOption = new SearchOption
+            var searchOption = new TelegramSearchBot.Model.SearchOption
             {
                 Count = 0,
                 Skip = 0,
@@ -58,7 +81,8 @@ namespace TelegramSearchBot.Tests.View
             };
 
             // Act
-            var result = SearchView.RenderSearchResults(messages, searchOption);
+            searchOption.Messages = messages;
+            var result = _searchView.RenderSearchResults(searchOption);
 
             // Assert
             Assert.AreEqual("未找到结果。\n", result);
@@ -70,20 +94,20 @@ namespace TelegramSearchBot.Tests.View
             // Arrange
             var messages = new List<Message>
             {
-                new Message { 
+                new Message {
                     Content = "Test content 1",
-                    GroupId = "-100123456789",
+                    GroupId = -100123456789,
                     MessageId = 123
                 },
-                new Message { 
+                new Message {
                     Content = "Test content 2",
-                    GroupId = "-100987654321",
+                    GroupId = -100987654321,
                     MessageId = 456
                 }
             };
 
             // Act
-            var result = SearchView.ConvertToMarkdownLinks(messages);
+            var result = _searchView.ConvertToMarkdownLinks(messages);
 
             // Assert
             Assert.AreEqual(2, result.Count);
@@ -99,15 +123,15 @@ namespace TelegramSearchBot.Tests.View
             // Arrange
             var messages = new List<Message>
             {
-                new Message { 
+                new Message {
                     Content = "Line1\nLine2\rLine3",
-                    GroupId = "-100123456789",
+                    GroupId = -100123456789,
                     MessageId = 123
                 }
             };
 
             // Act
-            var result = SearchView.ConvertToMarkdownLinks(messages);
+            var result = _searchView.ConvertToMarkdownLinks(messages);
 
             // Assert
             Assert.IsFalse(result[0].Contains("\n"));
