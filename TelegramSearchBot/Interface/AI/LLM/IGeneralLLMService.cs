fdda7077d415b86eece4f5f886e220809fe50c0d
ModerRAS <adqewsx@gmail.com>
2025-06-04 10:55:46 +0800

    清理一些无用的接口，只保留关键的

--- TelegramSearchBot/Interface/AI/LLM/IGeneralLLMService.cs
+++ TelegramSearchBot/Interface/AI/LLM/IGeneralLLMService.cs
@@ -11,16 +11,8 @@ namespace TelegramSearchBot.Interface.AI.LLM
     {
         string ServiceName { get; }
 
-        Task<List<LLMChannel>> GetChannelsAsync(string modelName);
-        IAsyncEnumerable<string> ExecAsync(Message message, long ChatId, CancellationToken cancellationToken = default);
         IAsyncEnumerable<string> ExecAsync(Message message, long ChatId, string modelName, ILLMService service, LLMChannel channel, CancellationToken cancellation);
-        IAsyncEnumerable<TResult> ExecOperationAsync<TResult>(Func<ILLMService, LLMChannel, CancellationToken, IAsyncEnumerable<TResult>> operation, string modelName, CancellationToken cancellationToken = default);
         Task<string> AnalyzeImageAsync(string PhotoPath, long ChatId, CancellationToken cancellationToken = default);
-        IAsyncEnumerable<string> AnalyzeImageAsync(string PhotoPath, long ChatId, string modelName, ILLMService service, LLMChannel channel, CancellationToken cancellationToken = default);
-        Task<float[]> GenerateEmbeddingsAsync(Message message, long ChatId);
         Task<float[]> GenerateEmbeddingsAsync(string message, CancellationToken cancellationToken = default);
-        IAsyncEnumerable<float[]> GenerateEmbeddingsAsync(string message, string modelName, ILLMService service, LLMChannel channel, CancellationToken cancellationToken = default);
-        Task<int> GetAltPhotoAvailableCapacityAsync();
-        Task<int> GetAvailableCapacityAsync(string modelName = "gemma3:27b");
     }
 }
